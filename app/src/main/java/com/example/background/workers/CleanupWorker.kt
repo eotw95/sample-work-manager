bea097a9cca91bf6efa196c40291b8812a17dddb
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-12-07 23:30:31 +0900

    Update CleanupWorker

--- app/src/main/java/com/example/background/workers/CleanupWorker.kt
+++ app/src/main/java/com/example/background/workers/CleanupWorker.kt
@@ -1,4 +1,36 @@
 package com.example.background.workers
 
-class CleanupWorker {
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.background.OUTPUT_PATH
+import java.io.File
+
+class CleanupWorker(ctx: Context, param: WorkerParameters): Worker(ctx, param) {
+    companion object {
+        private const val TAG = "CleanupWorker"
+    }
+    override fun doWork(): Result {
+        makeStatusNotification("Cleaning up old temporary files", applicationContext)
+
+        return try {
+            val outputDir = File(applicationContext.filesDir, OUTPUT_PATH)
+            if (outputDir.exists()) {
+                val entries = outputDir.listFiles()
+                for (entry in entries) {
+                    val fileName = entry.name
+                    if (fileName.isNotEmpty() && fileName.endsWith(".png")) {
+                        val deleted = entry.delete()
+                        Log.d(TAG, "Delete $fileName is $deleted")
+                    }
+                }
+            }
+            Result.success()
+        } catch(exception: Exception) {
+            exception.printStackTrace()
+            Result.failure()
+        }
+    }
+
 }
--- app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
+++ app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
@@ -1,4 +1,12 @@
 package com.example.background.workers
 
-class SaveImageToFileWorker {
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SaveImageToFileWorker(ctx: Context, param: WorkerParameters): Worker(ctx, param) {
+    override fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+
 }
